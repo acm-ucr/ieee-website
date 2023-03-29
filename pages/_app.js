075514271cfdd091cb31be6e24a406c410f17e90
menthy-wu <wumengxuan0728@gmail.com>
2023-03-29 00:08:53 -0700

    add eslint, prettier, and husky

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import React from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
