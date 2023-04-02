d679ee8b6434729d5c242b98944b004dbecc5919
menthy-wu <wumengxuan0728@gmail.com>
2023-04-01 19:49:26 -0700

    add board jsx

--- components/Board.jsx
+++ components/Board.jsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Board = () => {
-    return (
-        <div>
-            Board
-        </div>
-    );
+  return <div>Board</div>;
 };
 
 export default Board;
