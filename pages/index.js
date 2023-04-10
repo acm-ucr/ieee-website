4bd07d84d1105cdc969035e95091f978c2672ff7
Menthy <wumengxuan0728@gmail.com>
2023-04-09 21:53:48 -0700

    more setup

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-// import { Inter } from "next/font/google";
 import React from "react";
 import Landing from "@/components/Landing";
 import About from "@/components/About";
-// const inter = Inter({ subsets: ["latin"] });
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
+      <Image src="/assets/ieee-only-logo.svg" alt="pic" fill="t" />
       <Landing />
       <About />
     </div>
