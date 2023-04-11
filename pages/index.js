fa6fd687a63e24520172ccab37d850f6da91929f
Shing Hung <123037099+ysh0305@users.noreply.github.com>
2023-04-11 16:03:01 -0700

    clean up index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
 import React from "react";
 import Landing from "@/components/Landing";
 import About from "@/components/About";
-import Title from "@/components/Title";
-import JoinCard from "@/components/JoinCard";
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="bg-ieee-black">
-      <Title title="About IEEE" />
-      <JoinCard
-        color="ieee-purple"
-        name="Email us"
-        icon="email"
-        link="https://flowbite.com/docs/typography/links/"
-      />
+    <div>
       <Landing />
       <About />
     </div>
