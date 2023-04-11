32ebf81e5435daa9bfa223337e457415092bdab6
Shing Hung <123037099+ysh0305@users.noreply.github.com>
2023-04-10 21:55:34 -0700

    title :)

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@
 import React from "react";
 import Landing from "@/components/Landing";
 import About from "@/components/About";
+import Title from "@/components/Title";
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="bg-ieee-black">
+      <Title title="About IEEE" />
       <Landing />
       <About />
     </div>
