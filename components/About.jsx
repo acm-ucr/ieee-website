379cd7724c49dfafe3ae0a87a208dda78fa678b8
JakeCreate <developjake@gmail.com>
2023-04-11 16:09:31 -0700

    test lol

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import Title from "./Title";
 const About = () => {
-  return <div>About</div>;
+  return <div className="flex flex-col items-center">
+    <Title title="About IEE"/>
+    <p className="text-center w-9/12 font-giden">{'IEEE (pronounced "I triple E") is the Institute of Electrical and Electronics Engineers. We are a group dedicated to professionalism, connections, and our projects.'}</p>
+
+    <button className=''> checkout events </button>
+  </div>;
 };
 
 export default About;
