067a2d93d80a9207e7f59811d807b9d7f2fe1e8c
Menthy Wu <73259022+menthy-wu@users.noreply.github.com>
2023-04-11 17:01:04 -0700

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -9,18 +9,18 @@ export default function about() {
         <h2 className="font-teko">Who We Are?</h2>
       </div>
 
-      <p className="max-w-[60%] text-center py-3 font-giden">
+      <p className="max-w-[40%] text-center py-3 font-giden">
         Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.
       </p>
 
-      <img className="max-w-[70%] py-9" src={pic.src} alt="picture of students holding prizes"></img>
+      <img className="max-w-[50%] py-9" src={pic.src} alt="picture of students holding prizes"></img>
       
       <div className="flex justify-center pt-4">
         <img className="h-7 pr-2" src="/assets/title.svg" />
         <h2 className="font-teko">Our mission</h2>
       </div>
 
-      <p className="max-w-[60%] text-center py-3 font-giden">
+      <p className="max-w-[50%] text-center py-3 font-giden">
         We provide engineers with technical, social, and professional development in the engineering field. We aim to be the leading engineering organization on campus, providing more opprtunities for student improvement outside the classroom. We are commited in providing engineers with career enhancement, engineering social events, and growth in their respective professions.
       </p>
     </div>
