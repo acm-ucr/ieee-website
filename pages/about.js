7be43adffe43e866d44f8c62e8bc6a0bffc75703
Shing Hung <123037099+ysh0305@users.noreply.github.com>
2023-04-13 23:52:38 -0700

    fix Navbar, about Page format, Title format

--- pages/about.js
+++ pages/about.js
@@ -1,38 +1,37 @@
 import React from "react";
 import pic from "../public/pictures/index3.jpg";
+import Title from "@/components/Title";
 
 export default function about() {
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex justify-center pt-4">
-        <img className="h-7 pr-2" src="/assets/title.svg" />
-        <h2 className="font-teko">Who We Are?</h2>
+    <>
+      <img className=" rotate-45 absolute h-full" src="/assets/triangle.svg" />
+      <div className="flex w-full justify-center">
+        <div className="mt-3 w-1/2 flex items-center flex-col ">
+          <Title title="Who We Are" />
+
+          <p className="w-full my-10 text-center font-giden text-xl">
+            Lorem ipsum is placeholder text commonly used in the graphic, print,
+            and publishing industries for previewing layouts and visual mockups.
+          </p>
+          <img
+            className="w-full mt-10 mb-20"
+            src={pic.src}
+            alt="picture of students holding prizes"
+          ></img>
+
+          <Title title="Our Mission" />
+
+          <p className="w-full my-10 text-center font-giden text-xl">
+            We provide engineers with technical, social, and professional
+            development in the engineering field. We aim to be the leading
+            engineering organization on campus, providing more opprtunities for
+            student improvement outside the classroom. We are commited in
+            providing engineers with career enhancement, engineering social
+            events, and growth in their respective professions.
+          </p>
+        </div>
       </div>
-
-      <p className="max-w-[40%] text-center py-3 font-giden">
-        Lorem ipsum is placeholder text commonly used in the graphic, print, and
-        publishing industries for previewing layouts and visual mockups.
-      </p>
-
-      <img
-        className="max-w-[50%] py-9"
-        src={pic.src}
-        alt="picture of students holding prizes"
-      ></img>
-
-      <div className="flex justify-center pt-4">
-        <img className="h-7 pr-2" src="/assets/title.svg" />
-        <h2 className="font-teko">Our mission</h2>
-      </div>
-
-      <p className="max-w-[50%] text-center py-3 font-giden">
-        We provide engineers with technical, social, and professional
-        development in the engineering field. We aim to be the leading
-        engineering organization on campus, providing more opprtunities for
-        student improvement outside the classroom. We are commited in providing
-        engineers with career enhancement, engineering social events, and growth
-        in their respective professions.
-      </p>
-    </div>
+    </>
   );
 }
