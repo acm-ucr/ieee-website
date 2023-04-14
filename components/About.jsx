fc0f6bdd87a9ef27f190a6f8b2a9bd98ec8e64dd
JakeCreate <developjake@gmail.com>
2023-04-14 12:27:26 -0700

    Fixes #10
    also removed className "bg-ieee-black" from index.js

--- components/About.jsx
+++ components/About.jsx
@@ -1,12 +1,38 @@
 import React from "react";
 import Title from "./Title";
 const About = () => {
-  return <div className="flex flex-col items-center">
-    <Title title="About IEE"/>
-    <p className="text-center w-9/12 font-giden">{'IEEE (pronounced "I triple E") is the Institute of Electrical and Electronics Engineers. We are a group dedicated to professionalism, connections, and our projects.'}</p>
+  return (
+    <div>
+      <div className="flex flex-col items-center">
+        <Title title="About IEEE" />
+        <p className="text-center text-xl w-9/12 m-3 font-giden">
+          {
+            'IEEE (pronounced "I triple E") is the Institute of Electrical and Electronics Engineers. We are a group dedicated to professionalism, connections, and our projects.'
+          }
+        </p>
+      </div>
 
-    <button className=''> checkout events </button>
-  </div>;
+      <img
+        src="/pictures/index2.jfif"
+        className="flex-auto w-2/3 rounded-tr-full rounded-br-full"
+      ></img>
+
+      <div className="flex flex-col items-center m-40">
+        <button className="bg-ieee-pink text-6xl font-teko shadow-[10px_-8px_0px_0px] shadow-ieee-blue p-1">
+          Checkout Events
+        </button>
+      </div>
+
+      <div className="flex flex-row-reverse">
+        <img
+          src="/pictures/index1.jfif"
+          className=" w-2/3 rounded-tl-full rounded-bl-full"
+        ></img>
+      </div>
+
+      <img src="/assets/wave.svg" className="rotate-180"></img>
+    </div>
+  );
 };
 
 export default About;
