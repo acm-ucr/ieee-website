c37843811ec3658ccbc7a8d598d6c304619de922
Divyank Shah <divyank.shah.2016@gmail.com>
2023-04-15 21:38:42 -0700

    add google

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "prettier"],
+  extends: ["plugin:react/recommended", "google", "prettier"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
