7e80451fb41cb9b4a9c8633a6b802db724ff7838
menthy-wu <wumengxuan0728@gmail.com>
2023-04-15 21:23:57 -0700

    clean up and format

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Landing from "@/components/Landing";
 import About from "@/components/About";
-import Title from "@/components/Title";
 
 export default function Home() {
   return (
     <div>
-      <Title title="About IEEE" />
       <Landing />
       <About />
     </div>
