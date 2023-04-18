03de77cba467773ae4a8e047e8088e2371585db1
Shing Hung <123037099+ysh0305@users.noreply.github.com>
2023-04-17 20:02:16 -0700

    fix title image

--- components/Title.jsx
+++ components/Title.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import TitleSVG from "../public/assets/TitleSVG.svg";
 
 const Title = ({ title }) => {
   return (
     <div className="mt-3 mb-3 h-8 flex justify-center items-baseline w-full">
-      <img className=" h-3/4" src="/assets/title.svg" />
+      <img className=" h-3/4" src={TitleSVG.src} />
       <p className=" text-white text-4xl font-teko ml-2">{title}</p>
     </div>
   );
