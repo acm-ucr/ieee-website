aa7bac11550481e991f8c1b15a3f186314c14891
issy16 <ichen064@ucr.edu>
2023-04-20 21:03:05 -0700

    add Gear.jsx with rotation function

--- components/Gear.jsx
+++ components/Gear.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { motion } from "framer-motion";
 
-function Gear() {
-  return <div>Gear</div>;
+function Gear({ src }) {
+  return (
+    
+      <motion.div        
+        style={{ display: "inline-block" }}
+        animate={{ rotate: 360 }}
+        transition={{ duration: 4, repeat: Infinity, ease: "linear" }}
+      >
+        <img src={src} alt="gear"/>
+      </motion.div>
+    
+  );
 }
 
 export default Gear;
