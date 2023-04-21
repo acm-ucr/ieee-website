b6deca79243576e459077f64cf6bc881953da5f5
issy16 <ichen064@ucr.edu>
2023-04-20 21:16:35 -0700

    use Gear.jsx in about.js

--- components/Gear.jsx
+++ components/Gear.jsx
@@ -3,15 +3,13 @@ import { motion } from "framer-motion";
 
 function Gear({ src }) {
   return (
-    
-      <motion.div        
-        style={{ display: "inline-block" }}
-        animate={{ rotate: 360 }}
-        transition={{ duration: 4, repeat: Infinity, ease: "linear" }}
-      >
-        <img src={src} alt="gear"/>
-      </motion.div>
-    
+    <motion.div
+      style={{ display: "inline-block" }}
+      animate={{ rotate: 360 }}
+      transition={{ duration: 5, repeat: Infinity, ease: "linear" }}
+    >
+      <img src={src} alt="gear" />
+    </motion.div>
   );
 }
 
