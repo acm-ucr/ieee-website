35fe68285e602d5b8032404114027cd8f68de7c6
nguyena537 <63137037+nguyena537@users.noreply.github.com>
2023-04-22 13:09:39 -0700

    add color props to Boardcard, update board information with names and positions

--- components/BoardCard.jsx
+++ components/BoardCard.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 
-const BoardCard = ({ name, position }) => {
+const BoardCard = ({ name, position, bgColor, shadowColor }) => {
+  const textColor = "text" + bgColor.slice(2);
+
   return (
-    <div className="col-md-auto relative m-10">
-      <h1 className="font-teko text-ieee-pinkPurple text-5xl m-0">
+    <div className="col-md-auto w-25 relative my-20">
+      <h1 className={`font-teko ${textColor} lg:text-5xl md:text-3xl  m-0`}>
         {position}
       </h1>
       <div className="flex flex-row">
-        <div className="bg-ieee-pinkPurple h-72 w-2 r-5 mr-5"></div>
-        <img className="h-72" src="board/cat.png"></img>
+        <div className={`${bgColor} h-auto w-2 r-5 mr-5`}></div>
+        <img className="w-1/2" src="board/cat.png"></img>
       </div>
-      <div className="bg-ieee-pinkPurple shadow-[10px_-8px_0px_0px] shadow-ieee-pink text-center px-3 pt-3 text-3xl font-teko w-40 min-w-max h-16 absolute top-72 left-24">
+      <div
+        className={`${bgColor} shadow-[10px_-8px_0px_0px] ${shadowColor} text-center px-[4%] pt-[3%] text-3xl font-teko w-1/3 min-w-max h-[15%] absolute left-1/4 -bottom-[5%]`}
+      >
         {name}
       </div>
     </div>
--- pages/board.js
+++ pages/board.js
@@ -5,17 +5,120 @@ export default function board() {
   return (
     <div className="container-fluid">
       <div className="row justify-content-md-center">
-        <BoardCard name="Chloe Georgiou" position="Chair" />
-        <BoardCard name="Bob" position="Vice Chair" />
-        <BoardCard name="Chloe Georgiou" position="Secretary" />
-        <BoardCard name="Chloe Georgiou the Second" position="Program Chair" />
+        <BoardCard
+          name="Chloe Georgiou"
+          position="Chair"
+          bgColor="bg-ieee-pinkPurple"
+          shadowColor="shadow-ieee-pink"
+        />
+        <BoardCard
+          name="Laiba Hasan"
+          position="Vice Chair"
+          bgColor="bg-ieee-blue"
+          shadowColor="shadow-ieee-lightblue"
+        />
       </div>
+
       <div className="row justify-content-md-center">
-        <BoardCard name="Chloe Georgiou" position="Historian" />
-        <BoardCard name="Chloe Georgiou" position="Treasurer" />
-        <BoardCard name="Chloe Georgiou" position="Coordinator" />
-        <BoardCard name="Chloe Georgiou" position="Chair" />
-        <BoardCard name="Chloe Georgiou" position="Chair" />
+        <BoardCard
+          name="Harrison Cooper"
+          position="Secretary/Webmaster"
+          bgColor="bg-ieee-purple"
+          shadowColor="shadow-ieee-bluePurple"
+        />
+        <BoardCard
+          name="Andro Zaki"
+          position="Treasurer"
+          bgColor="bg-ieee-blue"
+          shadowColor="shadow-ieee-bluePurple"
+        />
+        <BoardCard
+          name="Connor Stewart"
+          position="Professional Development"
+          bgColor="bg-ieee-pinkPurple"
+          shadowColor="shadow-ieee-pink"
+        />
+      </div>
+
+      <div className="row justify-content-md-center">
+        <BoardCard
+          name="Nicholas Kang"
+          position="Program Chair"
+          bgColor="bg-ieee-pink"
+          shadowColor="shadow-ieee-pinkPurple"
+        />
+        <BoardCard
+          name="Alexander Hickerson"
+          position="Program Chair"
+          bgColor="bg-ieee-bluePurple"
+          shadowColor="shadow-ieee-pink"
+        />
+      </div>
+
+      <div className="row justify-content-md-center">
+        <BoardCard
+          name="Sethya Pugal"
+          position="Robotics Coordinator"
+          bgColor="bg-ieee-blue"
+          shadowColor="shadow-ieee-lightblue"
+        />
+        <BoardCard
+          name="Sonny Ding"
+          position="Robotics Coordinator"
+          bgColor="bg-ieee-purple"
+          shadowColor="shadow-ieee-bluePurple"
+        />
+      </div>
+
+      <div className="row justify-content-md-center">
+        <BoardCard
+          name="Divyank Shah"
+          position="Solar Car Coordinator"
+          bgColor="bg-ieee-purple"
+          shadowColor="shadow-ieee-blue"
+        />
+        <BoardCard
+          name="Daisy Rojas Garcia"
+          position="Solar Car Coordinator"
+          bgColor="bg-ieee-pinkPurple"
+          shadowColor="shadow-ieee-bluePurple"
+        />
+      </div>
+
+      <div className="row justify-content-md-center">
+        <BoardCard
+          name="Benjamin Bravo"
+          position="Fundraising Chair"
+          bgColor="bg-ieee-blue"
+          shadowColor="shadow-ieee-bluePurple"
+        />
+        <BoardCard
+          name="Saraí Herrerra"
+          position="Fundraising Chair"
+          bgColor="bg-ieee-pink"
+          shadowColor="shadow-ieee-pinkPurple"
+        />
+      </div>
+
+      <div className="row justify-content-md-center">
+        <BoardCard
+          name="Anthony Villicana"
+          position="Historian"
+          bgColor="bg-ieee-bluePurple"
+          shadowColor="shadow-ieee-pink"
+        />
+        <BoardCard
+          name="Zinal Patel"
+          position="Robosub Coordinator"
+          bgColor="bg-ieee-pinkPurple"
+          shadowColor="shadow-ieee-pink"
+        />
+        <BoardCard
+          name="Sreekar Vattipalli"
+          position="Graduate Student Representative"
+          bgColor="bg-ieee-blue"
+          shadowColor="shadow-ieee-lightblue"
+        />
       </div>
     </div>
   );
