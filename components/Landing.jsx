0a54777eb8b10b7bf526f9ab24011071b6fc87e6
Bryce7832 <bryce.hojo@gmail.com>
2023-04-24 00:07:07 -0700

    Added margins and some svgs. Need to center svgs

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -1,18 +1,34 @@
 import React from "react";
-
+import undulate from "../public/assets/undulate.svg";
 const Landing = () => {
   return (
     <div>
-      <div className="flex justify-center h-24">
+      <img
+        className="z-0 -rotate-45 absolute -left-80 -top-20 w-[600px] h-[600px] "
+        src={undulate.src}
+      />
+      <img
+        className="z-0 rotate-12 absolute -right-20 top-24 w-[700px] h-[800px]"
+        src={undulate.src}
+      />
+      <img
+        className=" rotate-6 z-0 left-72 absolute bottom-56 w-[160px]"
+        src="/assets/gear-stroke-pink-blue.svg"
+      />
+      <img
+        className=" z-0 left-52 absolute bottom-48 w-[110px]"
+        src="/assets/gear-lightblue.svg"
+      />
+      <div className="flex justify-center h-24 mt-20">
         <img src="/assets/ieee-only-logo.svg"></img>
       </div>
       <div>
-        <h1 className="font-teko text-center">IEEE @ UCR</h1>
+        <h1 className="font-teko text-center mt-3">IEEE @ UCR</h1>
         <h1 className="font-teko text-center">
           Institute of Electrical and Electronics Engineers
         </h1>
       </div>
-      <img src="/assets/wave.svg" className="w-100"></img>
+      <img src="/assets/wave.svg" className="w-100 mt-20 "></img>
     </div>
   );
 };
