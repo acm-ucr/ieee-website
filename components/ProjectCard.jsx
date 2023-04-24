d306097f4ef1a424ed37793abfe2f602b7e00a1e
menthy-wu <wumengxuan0728@gmail.com>
2023-04-24 01:16:36 -0700

    fix color and hover animation

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,64 +1,52 @@
 import React from "react";
 import { motion } from "framer-motion";
-import {FaEnvelope, FaLink } from "react-icons/fa";
+import { FaEnvelope, FaLink } from "react-icons/fa";
 import { BsDiscord } from "react-icons/bs";
 
 const ProjectCard = ({
   name,
   colour,
+  textColour,
   logoIcon,
   linkOne,
   linkTwo,
   linkThree,
 }) => {
   return (
-    <div
-      className={` h-80 w-44 flex flex-col items-center ml-10 bg-${colour}`}
-    >
-      <p className={`font-teko text-9xl mb-18 mt-4`}>
+    <div className={` h-80 w-44 flex flex-col items-center0 ${colour}`}>
+      <div className="w-full aspect-square p-4 flex items-center justify-center">
         {logoIcon}
-      </p>
-      <div className={` w-full h-32 flex flex-col items-center bg-${`white`}`}>
-        <p
-          className={`item-center font-teko text-4xl mb-0 mt-2 text-${colour}`}
-        >
+      </div>
+      <div className={` w-full h-32 flex flex-col items-center bg-white`}>
+        <p className={`item-center font-teko text-4xl mb-0 mt-2 ${textColour}`}>
           {name}
         </p>
 
-        <div className={`w-36 h-0.5 mb-2 bg-${colour}`}></div>
+        <div className={`w-36 h-0.5 mb-2 ${colour}`}></div>
 
         <div className={`justify-center w-full h-12 flex flex-wrap flex-row`}>
           <motion.a
+            whileHover={{ scale: 1.2 }}
             transition={{ type: "spring", stiffness: 500 }}
             href={linkOne}
           >
-            <button
-              className={`pb-1 pt-1 w-19 font-teko text-4xl ml-1 mr-2 text-${colour}`}
-            >
-              <BsDiscord />
-            </button>
+            <BsDiscord className={`text-4xl m-1 ${textColour}`} />
           </motion.a>
 
           <motion.a
             transition={{ type: "spring", stiffness: 500 }}
+            whileHover={{ scale: 1.2 }}
             href={linkTwo}
           >
-            <button
-              className={`pb-1 pt-1 w-19 font-teko text-4xl ml-1 mr-2 text-${colour}`}
-            >
-              <FaEnvelope />
-            </button>
+            <FaEnvelope className={`text-4xl m-1 ${textColour}`} />
           </motion.a>
 
           <motion.a
             transition={{ type: "spring", stiffness: 500 }}
+            whileHover={{ scale: 1.2 }}
             href={linkThree}
           >
-            <button
-              className={`pb-1 pt-1 w-19 font-teko text-4xl ml-1 mr-2 text-${colour}`}
-            >
-              <FaLink />
-            </button>
+            <FaLink className={`text-4xl m-1 ${textColour}`} />
           </motion.a>
         </div>
       </div>
--- components/ProjectSection.jsx
+++ components/ProjectSection.jsx
@@ -1,59 +1,67 @@
 import ProjectCard from "./ProjectCard";
-import { FaReact } from "react-icons/fa";
+import solacor from "../public/assets/solarcar.svg";
+import UAS from "../public/assets/UAS.png";
+import robosub from "../public/assets/robosub.png";
+import Micromouse from "../public/assets/Micromouse.svg";
 
 const ProjectCards = [
-    {
-        name: "Project Name",
-        colour: 'ieee-bluePurple',
-        logoIcon: <FaReact/>,
-        linkOne: "https://legacy.reactjs.org/docs/getting-started.html",
-        linkTwo: "https://legacy.reactjs.org/docs/getting-started.html",
-        linkThree: "https://legacy.reactjs.org/docs/getting-started.html",
-    },
-    {
-        name: "Project Name",
-        colour: 'ieee-bluePurple',
-        logoIcon: <FaReact/>,
-        linkOne: "https://legacy.reactjs.org/docs/getting-started.html",
-        linkTwo: "https://legacy.reactjs.org/docs/getting-started.html",
-        linkThree: "https://legacy.reactjs.org/docs/getting-started.html",
-    },
-    {
-        name: "Project Name",
-        colour: 'ieee-bluePurple',
-        logoIcon: <FaReact/>,
-        linkOne: "https://legacy.reactjs.org/docs/getting-started.html",
-        linkTwo: "https://legacy.reactjs.org/docs/getting-started.html",
-        linkThree: "https://legacy.reactjs.org/docs/getting-started.html",
-    },
-    {
-        name: "Project Name",
-        colour: 'ieee-bluePurple',
-        logoIcon: <FaReact/>,
-        linkOne: "https://legacy.reactjs.org/docs/getting-started.html",
-        linkTwo: "https://legacy.reactjs.org/docs/getting-started.html",
-        linkThree: "https://legacy.reactjs.org/docs/getting-started.html",
-    },
+  {
+    name: "Solar Car",
+    textColour: "text-ieee-bluePurple",
+    colour: "bg-ieee-bluePurple",
+    logoIcon: <img src={solacor.src} alt="solar car" />,
+    linkOne: "https://legacy.reactjs.org/docs/getting-started.html",
+    linkTwo: "https://legacy.reactjs.org/docs/getting-started.html",
+    linkThree: "https://legacy.reactjs.org/docs/getting-started.html",
+  },
+  {
+    name: "UAS",
+    textColour: "text-ieee-pinkPurple",
+    colour: "bg-ieee-pinkPurple",
+    logoIcon: <img src={UAS.src} alt="UAS" />,
+    linkOne: "https://legacy.reactjs.org/docs/getting-started.html",
+    linkTwo: "https://legacy.reactjs.org/docs/getting-started.html",
+    linkThree: "https://legacy.reactjs.org/docs/getting-started.html",
+  },
+  {
+    name: "Robosub",
+    textColour: "text-ieee-darkblue",
+    colour: "bg-ieee-darkblue",
+    logoIcon: <img src={robosub.src} alt="robosub" />,
+    linkOne: "https://legacy.reactjs.org/docs/getting-started.html",
+    linkTwo: "https://legacy.reactjs.org/docs/getting-started.html",
+    linkThree: "https://legacy.reactjs.org/docs/getting-started.html",
+  },
+  {
+    name: "Micromouse",
+    textColour: "text-ieee-pink",
+    colour: "bg-ieee-pink",
+    logoIcon: <img src={Micromouse.src} alt="UAS" />,
+    linkOne: "https://legacy.reactjs.org/docs/getting-started.html",
+    linkTwo: "https://legacy.reactjs.org/docs/getting-started.html",
+    linkThree: "https://legacy.reactjs.org/docs/getting-started.html",
+  },
 ];
 
 const ProjectSection = () => {
-    return (
-      <div className=" pb-5 pt-5 bg-ieee-black flex justify-center w-full">
-        <div className=" w-2/3 flex items-center justify-evenly">
-          {ProjectCards.map((card, index) => (
-            <ProjectCard
-              key={index}
-              name = {card.name}
-              colour={card.colour}
-              logoIcon={card.logoIcon}
-              linkOne={card.linkOne}
-              linkTwo={card.linkTwo}
-              linkThree={card.linkThree}
-            />
-          ))}
-        </div>
+  return (
+    <div className=" pb-5 pt-5 bg-ieee-black flex justify-center w-full">
+      <div className=" w-2/3 flex items-center justify-evenly">
+        {ProjectCards.map((card, index) => (
+          <ProjectCard
+            key={index}
+            name={card.name}
+            colour={card.colour}
+            logoIcon={card.logoIcon}
+            linkOne={card.linkOne}
+            linkTwo={card.linkTwo}
+            textColour={card.textColour}
+            linkThree={card.linkThree}
+          />
+        ))}
       </div>
-    );
-  };
-       
+    </div>
+  );
+};
+
 export default ProjectSection;
