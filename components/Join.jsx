83db9b199cb7e52aa6138c898067f9d4a08dba42
Kush <kushmomaya@gmail.com>
2023-05-01 19:25:59 -0700

    Added mobile responsiveness for text and image, still need to fix join cards and svg

--- components/Join.jsx
+++ components/Join.jsx
@@ -1,34 +1,36 @@
 import React from "react";
 import Title from "@/components/Title";
 import JoinCards from "./JoinCards";
+import undulate from "../public/assets/undulate.svg";
+import { Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 const Join = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div className="flex flex-col justify-center items-center overflow-hidden">
       <Title title="Join IEEE@UCR" />
 
       <div className="text-center">
-        <p className="text-xl leading-12 font-medium font-giden">
+        <p className="font-medium font-giden text-sm md:text-md lg:text-lg xl:text-xl">
           To become a student member, follow us online and show up to any
           meetings, events, or projects that interest you.
         </p>
       </div>
-
-      <div className="relative w-full">
-        <img
-          className=" relative z-30 mx-auto w-full"
-          src="pictures/index4.png"
-        />
+      <Row className="w-full">
+        <Col className="my-2">
+          <img className="w-full" src="pictures/index4.png" />
+        </Col>
+      </Row>
+      <Row xl={12} md={9} sm={6} xs={3} className=" w-full justify-center">
         <JoinCards />
-        <img
-          className="absolute bottom-1/2 -right-1/2 w-full h-full z-10"
-          src="/assets/undulate.svg"
-        />
-
-        <img
-          className="absolute top-10 -left-1/2 w-full h-full z-10"
-          src="/assets/undulate.svg"
-        />
-      </div>
+      </Row>
+      <Row className="w-100">
+        <Col xs={3} md={6} xl={12} className="my-2">
+          <img className="z-10 rotate-12 absolute w-full" src={undulate.src} />
+        </Col>
+        <Col xs={3} md={6} xl={12} className="my-2">
+          <img className="z-10 -rotate-90 absolute w-full" src={undulate.src} />
+        </Col>
+      </Row>
     </div>
   );
 };
