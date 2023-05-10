530b27e263bd2eaccaef6bf005438255b25462e4
Divyank Shah <divyank.shah.2016@gmail.com>
2023-05-10 02:00:09 -0700

    nav is functional

---
+++ components/MenuItems.jsx
@@ -0,0 +1,24 @@
+import Nav from "react-bootstrap/Nav";
+import Link from "next/link";
+import { useState } from "react";
+
+const MenuItem = ({ key, text, link }) => {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <Nav.Link as={Link} href={link} eventKey={key}>
+      <div
+        className="w-fit"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <p className="opacity-70 hover:opacity-100 mb-0 text-white">{text}</p>
+        {hover && (
+          <div className="bg-gradient-to-r from-ieee-purple to-ieee-blue h-[2.5px] w-full" />
+        )}
+      </div>
+    </Nav.Link>
+  );
+};
+
+export default MenuItem;
