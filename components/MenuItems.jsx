dd70b6dbf7aa684f59f929c40bc473a1d3504b8f
menthy-wu <wumengxuan0728@gmail.com>
2023-05-20 22:51:47 -0700

    fix nav

--- components/MenuItems.jsx
+++ components/MenuItems.jsx
@@ -2,33 +2,44 @@ import Nav from "react-bootstrap/Nav";
 import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/router";
+import { motion } from "framer-motion";
 
-const MenuItem = ({ index, text, link }) => {
+const MenuItem = ({
+  index,
+  text,
+  selected,
+  Hovered,
+  onClick,
+  link,
+  onMouseOver,
+  onMouseOut,
+}) => {
   const [hover, setHover] = useState(false);
   const router = useRouter();
 
   return (
-    <Nav.Link as={Link} href={link} eventKey={index}>
+    <Nav.Link as={Link} href={link} eventKey={index} className="">
       <div
-        className="w-fit"
+        className="w-fit relative"
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
+        onMouseOver={onMouseOver}
+        onMouseOut={onMouseOut}
+        onClick={onClick}
       >
         <p
           className={`${
             hover || router.asPath === link ? "opacity-100" : "opacity-70"
-          }   mb-0 text-white`}
+          }   mb-0 text-white p-0`}
         >
           {text}
         </p>
-        <div
-          className={`bg-gradient-to-r ${
-            hover || router.asPath === link
-              ? "from-ieee-purple to-ieee-blue"
-              : ""
-          }  h-[2.5px] w-full`}
-        />
-        {!hover && <div className="h-[2.5px] w-full" />}
+        {selected && (
+          <motion.div
+            className="absolute bg-gradient-to-r from-ieee-purple to-ieee-blue h-[2.5px] w-full"
+            layoutId="underline"
+          />
+        )}
       </div>
     </Nav.Link>
   );
