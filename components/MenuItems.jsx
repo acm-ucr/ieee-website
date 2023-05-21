a0964ea747d65a1b5cc539652ae31f5485bf968f
Divyank Shah <divyank.shah.2016@gmail.com>
2023-05-20 18:44:40 -0700

    persistent nav

--- components/MenuItems.jsx
+++ components/MenuItems.jsx
@@ -1,9 +1,11 @@
 import Nav from "react-bootstrap/Nav";
 import Link from "next/link";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 const MenuItem = ({ index, text, link }) => {
   const [hover, setHover] = useState(false);
+  const router = useRouter()
 
   return (
     <Nav.Link as={Link} href={link} eventKey={index}>
@@ -12,10 +14,8 @@ const MenuItem = ({ index, text, link }) => {
         onMouseEnter={() => setHover(true)}
         onMouseLeave={() => setHover(false)}
       >
-        <p className="opacity-70 hover:opacity-100 mb-0 text-white">{text}</p>
-        {hover && (
-          <div className="bg-gradient-to-r from-ieee-purple to-ieee-blue h-[2.5px] w-full" />
-        )}
+        <p className={`${hover || router.asPath === link ? "opacity-100" : "opacity-70"}   mb-0 text-white`}>{text}</p>
+         <div className={`bg-gradient-to-r ${hover || router.asPath === link ? "from-ieee-purple to-ieee-blue" : ""}  h-[2.5px] w-full`} />
         {!hover && <div className="h-[2.5px] w-full" />}
       </div>
     </Nav.Link>
